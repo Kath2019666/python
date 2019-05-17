7181cc7af7d44d2ed19890adaafa06914569ddb0
Kath2019666 <50708646+Kath2019666@users.noreply.github.com>
2019-05-17 11:21:33 +0800

    Add files via upload

---
+++ study005.py
@@ -0,0 +1,43 @@
+
+password = ''  # 变量password用来保存输入的密码
+
+while password!='816':
+
+    password=input('尝试输入密码：')
+
+print('欢迎回家！')
+
+man = ''
+while man != '有':
+    man = input('有没有愿意为小龙女四的男人？')
+print('小龙女可以下山啦')
+
+a = 0
+while a<100 :
+    a=a+1
+    print(a*5)
+
+ for i in range(1,5):
+     print('明日复明日，明日何其多')
+
+
+  j = 1
+  while j <5:
+      print('明日复明日，明日何其多')
+      j = j +1
+
+#homework:
+  a = range(1,8)
+  for i in a:
+      if i != 4:
+          print(i)
+  a = 0
+  while a < 8:
+      a = a+1
+      if a != 4:
+          print(a)
+      
+
+
+
+
